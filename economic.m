4f090f3a53d2ec3e950ab05b8ef397859eaacb99
Reviakin E. A <reviakinea@gmail.com>
2012-12-20 03:38:41 +0200

    OSH and Economic sections

---
+++ economic.m
@@ -0,0 +1,18 @@
+Cob = 4300 
+Scmp = Cob * 0.1
+Scm = Scmp * 0.5
+Sob = Cob + Scm
+Srez = Cob * 0.3
+Spl = Cob * 0.15
+Str = Cob * 0.04
+Szs = Cob * 0.012
+K = Sob + Srez + Spl + Str + Szs
+
+Na = 0.08
+Ca = Na * Cob 
+Cpl = Na * Spl
+Capol = Ca + Cpl
+Czpt = 8 * 25
+Czp = Czpt * 1.4
+CM = Czpt * 0.8
+Cpr = K * 0.01
